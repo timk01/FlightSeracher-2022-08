37affe8c4c8938016703915468f51d2cfe769af9
Timone5 <khalimovtimur95@gmail.com>
2022-10-14 16:02:42 +0500

    002 Создал spring boot проект для телеграмм бота(echo) (#21)

---
+++ src/main/java/ru/otus/flightsearch/bot/component/FlightSearcherBot.java
@@ -0,0 +1,50 @@
+package ru.otus.flightsearch.bot.component;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import ru.otus.flightsearch.bot.config.BotConfig;
+
+@Slf4j
+@Component
+public class FlightSearcherBot extends TelegramLongPollingBot {
+    private final BotConfig config;
+
+    public FlightSearcherBot(BotConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+            sendMessage(chatId, messageText);
+        }
+    }
+
+    private void sendMessage(long chatId, String textToSend) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            log.error("Error during register bot", e);
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getToken();
+    }
+}
---
+++ src/main/java/ru/otus/flightsearch/bot/config/BotInitializer.java
@@ -0,0 +1,31 @@
+package ru.otus.flightsearch.bot.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import ru.otus.flightsearch.bot.component.FlightSearcherBot;
+
+@Slf4j
+@Component
+public class BotInitializer {
+    private final FlightSearcherBot bot;
+
+    public BotInitializer(FlightSearcherBot bot) {
+        this.bot = bot;
+    }
+
+    @EventListener({ContextRefreshedEvent.class})
+    public void init() {
+        try {
+            TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
+            api.registerBot(bot);
+            log.info("Flight Searcher bot started");
+        } catch (TelegramApiException e) {
+            log.error("Error during initialize bot", e);
+        }
+    }
+}
