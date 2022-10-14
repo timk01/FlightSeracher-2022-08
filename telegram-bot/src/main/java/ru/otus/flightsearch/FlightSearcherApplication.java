223db9e9911c2b43190a37559e94c57ef8c598d7
timk01 <timk01@mail.ru>
2022-10-14 14:40:47 +0300

    feat: project modules
    * flightSearcher, 003 springBoot creation, made layout and simple entity + db
    
    * "Flightsearcher, 003 Создать spring boot проект для API с бизнес-логикой: added logic"
    
    * "Flightsearcher, 003 Создать spring boot проект для API с бизнес-логикой: added logic"
    
    * "Flightsearcher, 003 create a spring project layout, remade it on 3 modules, changed original pom, made a compile (compile works, but not test)"
    
    Co-authored-by: Oleg Agafonov <oleg.a.agafonov@gmail.com>

---
+++ telegram-bot/src/main/java/ru/otus/flightsearch/FlightSearcherApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.flightsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightSearcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightSearcherApplication.class, args);
+    }
+
+}
