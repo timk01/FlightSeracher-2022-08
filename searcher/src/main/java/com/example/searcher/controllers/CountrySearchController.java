c19ffe2c17243e91a0ef03ee817e730477a8a59e
egorkomarov <egorkom@gmail.com>
2022-10-25 18:45:48 +0400

    #25 small fixes

--- searcher/src/main/java/com/example/searcher/controllers/CountrySearchController.java
+++ searcher/src/main/java/com/example/searcher/controllers/CountrySearchController.java
@@ -3,14 +3,13 @@ package com.example.searcher.controllers;
 import com.example.searcher.dtos.CountryDto;
 import com.example.searcher.service.CountrySearchService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 
 @RestController
-@RequestMapping("api/countrysearch")
+@RequestMapping("api/country-search")
 @RequiredArgsConstructor
 public class CountrySearchController {
 
@@ -18,7 +17,6 @@ public class CountrySearchController {
 
     @GetMapping
     public List<CountryDto> getCountries() {
-
         return countrySearchService.getCountry();
     }
 }
